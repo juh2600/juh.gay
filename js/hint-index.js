dcdda5ef20b96a7494df949ea0fe2c145a67586d
Joe Reed <josephreed2600@gmail.com>
2023-02-19 15:49:17 -0800

    autoindex (28)

--- js/hint-index.js
+++ js/hint-index.js
@@ -6,7 +6,11 @@ addEventListener('DOMContentLoaded', () => {
 			if (res.ok) {
 				let main = document.querySelector('main');
 				let nav = document.createElement('nav');
-				nav.innerHTML = `There's a directory here. <a href="${subdir}">Explore it?</a>`;
+				if (Math.random() < 0.98) {
+					nav.innerHTML = `There's a directory here. <a href="${subdir}">Explore it?</a>`;
+				} else {
+					nav.innerHTML = `You are standing in a page of content south of a nav bar, with poor SEO. <a href="${subdir}">There is a small directory here.</a>`;
+				}
 				nav.classList.add('subdir-nav');
 				main.prepend(nav);
 			}
