4756ca31ba7dbf1bf0627e42c5f1a54ed3285499
Joe Reed <josephreed2600@gmail.com>
2023-02-19 15:40:54 -0800

    autoindex (27)

--- js/autoindex.js
+++ js/autoindex.js
@@ -4,7 +4,6 @@ const dir = path.split('/').slice(-2,-1);
 const updateFileTitles = () => {
 	document.querySelectorAll('[data-autoindex-title-filename]').forEach(node => {
 		let filename = node.dataset.autoindexTitleFilename;
-		console.log(filename, node);
 		fetch(filename, {headers: {range: 'bytes=0-4095'}})
 			.then(res => res.text())
 			.then(text => new DOMParser().parseFromString(text, 'text/html').title.split(' | ').slice(0,-1).join(' | '))
