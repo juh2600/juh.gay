c6c8d1050c4d75242e0b05bbf519ee2af4faee6b
Joe Reed <josephreed2600@gmail.com>
2023-02-19 11:40:49 -0800

    rearrange things and do evil

--- build/build.fish
+++ build/build.fish
@@ -1,11 +1,17 @@
 #!/usr/bin/fish
 
+# this should be run from top level of repo, e.g., `build/build.fish'
+# (because the files/dirs referenced below are relative to top of repo)
+
 set -x indir src/
 set -x outdir doc/
 set -x resources js css files
+set -x nginx_files xslt
+set -x nginx_files_target /tmp/nginx
 
 # make clean
 rm -rf $outdir
+rm -rf $nginx_files_target
 
 # duplicate directory structure
 for subdir in (find src/ -type d)
@@ -29,9 +35,17 @@ for infile in (find src/ -type f | grep '\.md$')
 	$cmd | sed -Ee 's@<a (href=[^>]+://[^>]+>)@<a target=_blank \1@g' > $outfile # sed compels external links to open in new tabs
 end
 
-# link resources into server-accessible space
+# link web resources into server-accessible space
 for resource in $resources
 	set -x cmd ln -s ../$resource $outdir
+	echo $cmd
+	$cmd
+end
+
+# link files that nginx wants but that shouldn't be served into /tmp lol this is bad
+mkdir $nginx_files_target
+for thing in $nginx_files
+	set -x cmd ln -s (realpath $thing) $nginx_files_target
 	echo $cmd
 	$cmd
 end
